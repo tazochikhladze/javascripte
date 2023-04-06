791711f329224063282fbc9899521623bdcbe134
Tazo Chikhladze <tazochikhladze23@gmail.com>
2023-04-06 17:27:23 +0400

    js homework

---
+++ index6.js
@@ -0,0 +1,10 @@
+
+const  userpaid = 2090 ;
+const conwesTer = 2090 ;
+const erowdent = 3023 ;
+const bertconsol = 3023 ;
+
+const pinegange = userpaid == conwesTer && erowdent == bertconsol;
+
+
+console.log (pinegange);
